2a9e9e2886899f9acfbee620c570a512d518ecbb
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2020-04-07 20:28:17 -0700

    Creating Api

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new PostCollection(Post::paginate(5));
+    }
+}
